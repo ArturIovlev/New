faeb23a8ff8e837543341901acbd07c1320451cb
ArturIovlev <74561570+ArturIovlev@users.noreply.github.com>
2021-01-11 19:21:30 +0300

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# scaffolded controller for accounts
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
   skip_before_action :require_login, only: %i[new create]
